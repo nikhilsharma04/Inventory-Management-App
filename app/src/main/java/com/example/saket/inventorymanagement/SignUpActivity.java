1ee476aa16adede46f6da8b5ed27196852c5e81b
suplex.saket@gmail.com <saket.singh1410@gmail.com>
2016-12-11 04:53:57 +0530

    SignUpActivity Added

---
+++ app/src/main/java/com/example/saket/inventorymanagement/SignUpActivity.java
@@ -0,0 +1,103 @@
+package com.example.saket.inventorymanagement;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+/**
+ * Created by saket on 12/11/16.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+    private String email , pass;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        inputEmail = (EditText)findViewById(R.id.email);
+        inputPassword = (EditText)findViewById(R.id.pass);
+        btnSignIn = (Button)findViewById(R.id.sign_in_button);
+        btnSignUp = (Button)findViewById(R.id.sign_up_button);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        auth = FirebaseAuth.getInstance();
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = inputEmail.getText().toString().trim();
+                pass = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getBaseContext() , "Enter email Address" , Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pass)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (pass.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                auth.createUserWithEmailAndPassword(email , pass)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    Toast.makeText(SignUpActivity.this ,"Successfully registered" , Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(SignUpActivity.this , MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+}
